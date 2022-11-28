83528ec197948e9e5c0f0fd7d0f380cf6ce344a3
Robin Bolscher <robinbolscher@gmail.com>
2022-11-28 12:33:48 +0100

    feat: add basic types for Struct
    This allows using this module in TypeScript projects.

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+interface StructInstance<T> {
+  fromBuffer: (buffer: Buffer) => T; // Object
+  toBuffer: (object: T, index?: number) => Buffer; // Object
+}
+
+declare module "@athombv/data-types" {
+  const DataTypes: any;
+  const Struct: <Type>(name: string, objectDefinition: Type) => StructInstance<Type>;
+
+  export { DataTypes, Struct };
+}
+
+/*
+How to use @athombv/data-types in TypeScript:
+
+// Create a type that represents the Struct data
+type ZdoEndDeviceAnnounceIndication = {
+  srcAddr: number;
+  IEEEAddr: string;
+};
+
+// Create a Struct instance with generic type ZdoEndDeviceAnnounceIndication
+const ZdoEndDeviceAnnounceIndicationStruct =
+  Struct<ZdoEndDeviceAnnounceIndication>("ZdoEndDeviceAnnounceIndication", {
+    srcAddr: DataTypes.uint16,
+    IEEEAddr: DataTypes.EUI64,
+  });
+
+// Create ZdoEndDeviceAnnounceIndication object
+const ZdoEndDeviceAnnounceObject = ZdoEndDeviceAnnounceIndicationStruct.fromBuffer(
+  Buffer.from([0, 1, 2, 3])
+);
+
+ZdoEndDeviceAnnounceObject.srcAddr.trim(); // This errors, srcAddr is not a string
+
+// Create Buffer instance from ZdoEndDeviceAnnounceObject
+const ZdoEndDeviceAnnounceBuffer = ZdoEndDeviceAnnounceIndicationStruct.toBuffer({ srcAddr: 1, IEEAddr: 'abc' }); // This errors due to typo in IEEEAddr name
+*/
