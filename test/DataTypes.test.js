a30a7fa13ca8c2e29cc69f392c43bf5c248c6d05
Robin Bolscher <robinbolscher@gmail.com>
2023-02-27 20:24:38 +0100

    test: add test for bool and uint8 types

--- test/DataTypes.test.js
+++ test/DataTypes.test.js
@@ -3,28 +3,51 @@
 const assert = require('assert').strict;
 const { DataTypes } = require('..');
 
-const bits = ['bit1', 'bit2', 'bit3', 'bit4'];
-const bitValue = 0b0101;
-const testMap = DataTypes.map8(...bits)
-  .fromBuffer(Buffer.of(bitValue));
-
-const bufferSize = 8;
-const bufferOffset = 4;
-const expectedBuffer = Buffer.of(0, 0, 0, 0, bitValue, 0, 0, 0);
-
-describe('DataType', function() {
+describe('DataTypes', function() {
+  describe('bool', function() {
+    it('should parse to buffer', function() {
+      const buffer = Buffer.from([0]);
+      DataTypes.bool.toBuffer(buffer, true, 0);
+      assert.deepEqual(buffer, Buffer.from([1]));
+    });
+    it('should parse from buffer', function() {
+      assert.strictEqual(DataTypes.bool.fromBuffer(Buffer.from([1]), 0), true);
+    });
+  });
+  describe('uint8', function() {
+    it('should parse to buffer', function() {
+      const buffer = Buffer.from([0]);
+      DataTypes.uint8.toBuffer(buffer, 12, 0);
+      assert.deepEqual(buffer, Buffer.from([12]));
+    });
+    it('should parse from buffer', function() {
+      assert.strictEqual(DataTypes.uint8.fromBuffer(Buffer.from([12]), 0), 12);
+    });
+  });
   describe('map8', function() {
+    const bits = ['bit1', 'bit2', 'bit3', 'bit4'];
+    const bitValue = 0b0101;
+    const bufferSize = 8;
+    const bufferOffset = 4;
+    let testMap;
+    let expectedBuffer;
+    beforeEach(function() {
+      testMap = DataTypes.map8(...bits)
+        .fromBuffer(Buffer.of(bitValue));
+      expectedBuffer = Buffer.of(0, 0, 0, 0, bitValue, 0, 0, 0);
+    });
+
     it('should parse to buffer', function() {
       // Test non-static toBuffer
       const buffer = Buffer.alloc(bufferSize);
       testMap.toBuffer(buffer, bufferOffset);
-      assert.deepEqual(buffer, expectedBuffer, 'Non-static toBuffer failed');
+      assert.deepEqual(buffer, expectedBuffer);
     });
     it('should parse static to buffer', function() {
       // Test static toBuffer
       const buffer = Buffer.alloc(bufferSize);
       testMap.constructor.toBuffer(buffer, bufferOffset, testMap.length, bits, testMap);
-      assert.deepEqual(buffer, expectedBuffer, 'Static toBuffer failed');
+      assert.deepEqual(buffer, expectedBuffer);
     });
   });
 });
