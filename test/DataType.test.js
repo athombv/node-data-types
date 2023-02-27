03d8bca24aae8c8d4b5cdccc4d869d4614f5c122
Robin Bolscher <robinbolscher@gmail.com>
2023-02-27 19:42:26 +0100

    test: add basic tests for DataType instance properties

---
+++ test/DataType.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('assert').strict;
+const { DataType } = require('..');
+
+describe('DataType', function() {
+  it('instance should have properties [id,shortName,length,toBuffer,fromBuffer,args,defaultValue,isAnalog]', function() {
+    const dataType = new DataType(0, 'dummyType', 1, () => Buffer.from([0]), () => 0, 'args1', 'args2');
+    assert.equal(dataType.id, 0);
+    assert.equal(dataType.shortName, 'dummyType');
+    assert.equal(dataType.length, 1);
+    assert.equal(dataType.defaultValue, 0);
+    assert.equal(dataType.isAnalog, true);
+    assert.deepEqual(dataType.args, ['args1', 'args2']);
+  });
+});
